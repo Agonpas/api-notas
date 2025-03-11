984b1229c41a5c79fde6272b0b8e8eb059f14c31
Agonpas <agonpas@gmail.com>
2025-03-11 10:12:45 +0100

    Rutas API estudiantes, asignaturas y notas creados 	modified:   routes/api.php

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,14 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\EstudianteController;
+use App\Http\Controllers\AsignaturaController;
+use App\Http\Controllers\NotaController;
+
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::apiResource('estudiantes', EstudianteController::class);
+Route::apiResource('asignaturas', AsignaturaController::class);
+Route::apiResource('notas', NotaController::class);
